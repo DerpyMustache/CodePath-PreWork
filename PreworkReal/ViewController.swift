1d9e8d8b3d83534182398d86d9080e5c0b62f090
DerpyMustache <49598834+DerpyMustache@users.noreply.github.com>
2021-12-28 15:57:09 -0500

    functionality update
    Added split feature

--- PreworkReal/ViewController.swift
+++ PreworkReal/ViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var tipSplitTextField: UITextField!
     @IBOutlet weak var billAmountTextField: UITextField!
     @IBOutlet weak var tipControl: UISegmentedControl!
     @IBOutlet weak var tipAmountLabel: UILabel!
@@ -22,8 +24,9 @@ class ViewController: UIViewController {
     @IBAction func calculateTip(_ sender: Any) {
         let bill = Double(billAmountTextField.text!) ?? 0
         let tipPercentages = [0.15,0.18,0.2]
-        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
-        let total = bill + tip
+        let split = Double(tipSplitTextField.text!) ?? 1
+        let tip = (bill * tipPercentages[tipControl.selectedSegmentIndex])/split
+        let total = bill + tip*split
         
         tipAmountLabel.text = String(format: "$%.2f", tip)
         totalLabel.text = String(format: "$%.2f", total)
